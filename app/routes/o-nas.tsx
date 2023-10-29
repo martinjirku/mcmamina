7b3b75e31f6d4eef02ca34f48a7b343a24a31540
harryjrk <martin@jirku.sk>
2023-10-29 21:38:32 +0100

    reference old vtour

--- app/routes/o-nas.tsx
+++ app/routes/o-nas.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "~/components/layout";
 import backgroundImage from "~/images/crayons-1445053_640.jpg";
 
+// https://www.mcmamina.sk/nova_stranka/vtour/
 export default function ONasPage() {
   return (
     <Layout
@@ -9,7 +10,7 @@ export default function ONasPage() {
     >
       <div className="w-full bg-indigo-100 bg-opacity-80 text-xl leading-10 flex justify-around py-12 my-12 md:py-16 md:my-16 xl:py-20 xl:my-28">
         <iframe
-          src="http://mcmamina.panfoto.sk/"
+          src="https://www.mcmamina.sk/nova_stranka/vtour/"
           title="Virtuálna prehliadka"
           className="w-4/5 h-96"
           allowFullScreen
