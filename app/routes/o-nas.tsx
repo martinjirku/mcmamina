2f544fc9836e13373f0e55b68da9b53eb0d8a8a0
harryjrk <martin@jirku.sk>
2023-11-29 21:23:38 +0100

    o nas sekcia

--- app/routes/o-nas.tsx
+++ app/routes/o-nas.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "~/components/layout";
 import backgroundImage from "~/images/crayons-1445053_640.jpg";
+import { FullWidthCard, CardContent } from "~/components/fullWidthCard";
 
 // https://www.mcmamina.sk/nova_stranka/vtour/
 export default function ONasPage() {
@@ -8,14 +9,40 @@ export default function ONasPage() {
       className="w-full bg-cover bg-center text-indigo-800 font-light"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="w-full bg-indigo-100 bg-opacity-80 text-xl leading-10 flex justify-around py-12 my-12 md:py-16 md:my-16 xl:py-20 xl:my-28">
+      <FullWidthCard
+        background="bg-indigo-950"
+        padding="px-0 md:px-5"
+        margin="mt-0"
+      >
         <iframe
           src="https://www.mcmamina.sk/nova_stranka/vtour/"
           title="Virtuálna prehliadka"
-          className="w-4/5 h-96"
+          className="w-full lg:w-4/5 h-96"
           allowFullScreen
         ></iframe>
-      </div>
+      </FullWidthCard>
+      <FullWidthCard
+        background="bg-indigo-200"
+        padding="px-0 pt-12 pb-24 md:px-5"
+        margin="mt-0 mb-24"
+      >
+        <CardContent className="text-indigo-800">
+          <h1 className="text-4xl font-semibold mb-10 text-center">
+            Niečo o nás
+          </h1>
+          <p className="text-xl text-left">
+            Materské centrum Mamina je nezisková organizácia, ktorá vznikla v
+            roku 2004. Vznikla ako potreba stretávať sa v bezpečnom prostredí a
+            zmysluplne tráviť čas so svojimi deťmi na materskej, či rodičovskej
+            dovolenke. Naše Materské centrum tento rok oslávilo 19té narodeniny
+            Za ten čas sme my, dobrovoľníčky, vybudovali nádhernú herňu,
+            organizujeme krúžky, veľké akcie ako karneval, Míľa pre mamu,
+            Mikuláš, prednášky, podporné skupiny,...Tvoríme komunitu, kde si
+            každý nájde, to, čo potrebuje: zábavu, oddych, sebarealizáciu,
+            informácie a hlavne krásny čas so svojim dieťaťom.
+          </p>
+        </CardContent>
+      </FullWidthCard>
     </Layout>
   );
 }
