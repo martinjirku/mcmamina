e48e9ebefae4731780c073310af888219ce70838
Martin Jirku <martin@jirku.sk>
2023-12-10 15:49:19 +0100

    remove old code
    Signed-off-by: Martin Jirku <martin@jirku.sk>

--- app/routes/podpora.dobrovolnictvo.tsx
+++
@@ -1,9 +0,0 @@
-import { FullWidthCard, CardContent } from "~/components/fullWidthCard";
-
-export default function Index() {
-  return (
-    <FullWidthCard>
-      <CardContent className="leading-normal"></CardContent>
-    </FullWidthCard>
-  );
-}
