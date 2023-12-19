6319fe063b72feaa872d29fa074b33184274ac89
Martin Jirku <martin@jirku.sk>
2023-12-19 07:58:30 +0100

    volunteering page

---
+++ handlers/Volunteers.go
@@ -0,0 +1,25 @@
+
+package handlers
+
+import (
+	"log/slog"
+	"net/http"
+
+	"jirku.sk/mcmamina/template/components"
+	"jirku.sk/mcmamina/template/pages"
+)
+
+func Volunteers(Log *slog.Logger, cssPathGetter CSSPathGetter) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		Log.Info("request", slog.String("method", r.Method), slog.String("path", r.URL.Path))
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		cssPath, _ := cssPathGetter.GetCssPath()
+		// TODO: handle GET
+		components.Page(components.NewPage(
+			"Volunteers",
+			"Dobrovoľníci",
+			cssPath,
+			pages.VolunteersPage(),
+		)).Render(r.Context(), w)
+	}
+}
