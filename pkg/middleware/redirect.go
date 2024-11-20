5e02bcf5347d319058170e7ed155f3e9b7276f14
Martin Jirku <martin@jirku.sk>
2024-11-20 15:53:09 +0100

    redirect from www to the root

---
+++ pkg/middleware/redirect.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func RedirectFromWWWSubdomain(logger *slog.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Host == "www.mcmamina.sk" {
+				if r.URL.RawQuery != "" {
+					target := "https://mcmamina.sk"
+					// target := fmt.Sprintf("https://mcmamina.sk%s", r.URL.Path)
+					// target += "?" + r.URL.RawQuery
+					logger.Info("redirecting", slog.String("host", r.Host), slog.String("target", target))
+					http.Redirect(w, r, target, http.StatusMovedPermanently)
+					return
+				}
+
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
